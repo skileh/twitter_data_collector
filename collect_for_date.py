61308edcad89d0b99e640ca391e5d9d1523ac79a
skileh <38012479+skileh@users.noreply.github.com>
2021-10-05 12:03:21 -0300

    Update collect_for_date.py
    Count is related to the amount of tweets that must be retrieved, keyword is related to the user's user_name.
    For implementation, I used twitter's V2 api along with Tweepy 4.0.
    It's recommended to use pip install tweepy to download the latest available tweepy version which supports twitter v2 tool.

--- collect_for_date.py
+++ collect_for_date.py
@@ -1,15 +1,21 @@
+pip install git+https://github.com/tweepy/tweepy.git
+  
 import re
 import tweepy
 from datetime import datetime
 from textblob import TextBlob
+import json
+import requests
+import numpy as np
+import pandas as pd
 
 CONSUMER_KEY = consumer_key
 CONSUMER_SECRET = consumer_secret
 ACCESS_TOKEN = acess_token
 ACCESS_TOKEN_SECRET = acess_token_secret
 
-keyword = ("beyonce")
-count = 10
+keyword = ('keyword')
+count = 100
 
 class TweetAnalyzer():
 
@@ -23,7 +29,7 @@ class TweetAnalyzer():
     
 
 
-    self.conToken = tweepy.Client(auth)
+    self.conToken = tweepy.Client(barrer,auth)
 
   def __clean_tweet(self, tweets_text):
     '''
@@ -37,48 +43,36 @@ class TweetAnalyzer():
 
     return clean_text
 
-  def search_by_keyword(self, keyword, count=10, result_type='mixed', lang='en', tweet_mode='extended'):
+  def search_by_keyword(self, keyword, count=100, result_type='mixed', lang='en', tweet_mode='extended'):
     '''
       Search for the twitters thar has commented the keyword subject.
+    
     '''
-    ID = self.conToken.get_user(username =keyword)
-    tweets_iter = self.conToken.get_users_tweets(
+    nome = self.conToken.get_user(username = keyword)
+   # print(nome.data.id)
+    print(nome)
+    ID=nome.data.id
+    tweets_iter = self.conToken.get_users_tweets(  
+                                                   tweet_fields=['context_annotations', 'created_at'],
                                                    id=ID,
-                                                   end_time =datetime(2020,1,31,0,0,0).date() ,
-                                                   start_time =datetime(2019,7,31,0,0,0).date(),
-                                                   max_results=count
-                                                   )
-
-    return tweets_iter
+                                                   end_time ='2021-01-28T00:00:01Z' ,
+                                                   start_time ='2018-07-28T00:00:01Z',
+                                                   max_results=100
+                                                )
+    return tweets_iter.data
 
   def prepare_tweets_list(self, tweets_iter):
     '''
       Transforming the data to DataFrame.
     '''
-
+   
     tweets_data_list = []
     for tweet in tweets_iter:
-      if not 'retweeted_status' in dir(tweet):
-        tweet_text = self.__clean_tweet(tweet.full_text)
-        tweets_data = {
-            'len' : len(tweet_text),
-            'ID' : tweet.id,
-            'User' : tweet.user.screen_name,
-            'UserName' : tweet.user.name,
-            'UserLocation' : tweet.user.location,
-            'TweetText' : tweet_text,
-            'Language' : tweet.user.lang,
-            'Date' : tweet.created_at,
-            'Source': tweet.source,
-            'Likes' : tweet.favorite_count,
-            'Retweets' : tweet.retweet_count,
-            'Coordinates' : tweet.coordinates,
-            'Place' : tweet.place 
-        }
-
-        tweets_data_list.append(tweets_data)
-
-    return tweets_data_list
+     # if not 'retweeted_status' in dir(tweet):
+        tweets_data_list.append([tweet.id,tweet.text])
+    #print(tweets_data_list)
+    df = pd.DataFrame(tweets_data_list,columns=['id','text'])
+    return df
 
 analyzer = TweetAnalyzer(consumer_key = CONSUMER_KEY, consumer_secret = CONSUMER_SECRET, 
 access_token = ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
@@ -86,4 +80,4 @@ access_token = ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
 # Para realizar as buscas usando a keyword e quantidade predefinida e converter em uma lista.
 tweets_iter = analyzer.search_by_keyword(keyword, count)
 tweets_list = analyzer.prepare_tweets_list(tweets_iter)
-print(tweets_list)
+tweets_list.count()
